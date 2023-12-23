9b6c8aff60083b18f659cdc85c103510b8e941d1
Jokul482 <45899547+Jokul482@users.noreply.github.com>
2023-12-23 23:48:08 +0800

    Navbar修改

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -7,13 +7,16 @@ const menu = blog.layout;
     <div class="flex space-x-4 text-sm items-center">
         <template v-for="(item, i) in menu.menus" :key="i">
             <nuxt-link :to="item.to" class="">
-                <span class="text-gray-700 dark:text-gray-300">{{ item.title }}</span>
+                <span class="text-primary">{{ item.title }}</span>
             </nuxt-link>
         </template>
     </div>
     <div class="pl-4 flex space-x-3 text-xl">
-        <div class="flex items-center"></div>
-        <nuxt-link :to="link.github" class="dark:text-gray-400 text-gray-600">
+        <!-- <div class="flex items-center">
+            <Icon name="charm:sun" class="text-primary"></Icon>
+            <Icon name="akar-icons:moon-fill" class="text-primary"></Icon>
+        </div> -->
+        <nuxt-link :to="link.github" class="text-primary">
             <Icon name="grommet-icons:github" />
         </nuxt-link>
     </div>
